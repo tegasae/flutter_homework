999dd19d697b3be5c2dc13c91ed81a971a778bd6
sae <sae@tega.ru>
2021-10-29 18:53:21 +0300

    test for exercise2

--- homework1/lib/exercise2.dart
+++ homework1/lib/exercise2.dart
@@ -1,20 +1,36 @@
-class DecBin {
+//упражнение 2
+//Реализуйте методы для преобразования целых чисел из десятичной системы в двоичную и обратно.
 
+class DecBin {
+  //перевод из дестичной в двоичную
   static int dec2bin(int number) {
+
     int result = 0;
     int p = 1;
+
+    if (number<0) {
+      //только больше нуля
+      //инвертировать и прибавлять единицу
+      throw ArgumentError('Only great or equal at 0');
+    }
     while (number > 0) {
       result = result + (number % 2) * p;
       number = (number / 2).floor();
       p *= 10;
     }
+
     return result;
   }
-
+  //перевод из двоичной в десятичную
+  //интересно было попробовать средствами языка
   static int bin2dec(int number) {
     //print(55.toRadixString(2)); // Outputs 110111
     //print(int.parse("110111", radix: 2)); //Outputs 55
     //print(num.toRadixString(10));
-    return int.parse(number.toString(), radix: 2);
+    try {
+      return int.parse(number.toString(), radix: 2);
+    } on FormatException catch(e) {
+      throw ArgumentError('Only 0 or 1');
+    }
   }
 }
--- homework1/main.dart
+++ homework1/main.dart
@@ -1,7 +1,7 @@
 import 'package:test/test.dart';
 
 import 'package:homework1/exercise1.dart';
-import 'package:homework1/exercise2.dart';
+import 'package:homework1/exercise2.dart' as Exercise2;
 import 'package:homework1/exercise3.dart';
 import 'package:homework1/exercise4.dart';
 import 'package:homework1/exercise5.dart';
@@ -10,25 +10,34 @@ import 'package:homework1/exercise7.dart';
 import 'package:homework1/exercise8.dart';
 import 'package:homework1/exercise9.dart';
 
+
+
 main() {
-  //Calculate calculate=Calculate(102);
-  //print(calculate.gcd(5));
-  //print(calculate.lcm(5));
-  //print(calculate.findSimple());
-  //exercise 4
-  //print(CounterWords.counter(['wfrf','erfverver','word','verv','word']));
-  //exercise 5
-  //print(CounterNumbers.counterNumbers(['one, two, one, four, fwrf. zero, zero', 'nine, zero, erfwrf']));
-
-  //m('2*2');
-
-  test('String.split() splits the string on the delimiter', () {
-    var string = 'foo,bar,baz';
-    expect(string.split(','), equals(['foo', 'bar', 'baz']));
+  //exercise2
+  group('Упражнение 1', () {
+    var zero=0;
+    var n1024=1024;
+    var b256=100000000;
+    var notBin=123;
+    test('Перевод десятичного числа '+ zero.toString()+ 'в двоичное ', () {
+      expect(Exercise2.DecBin.dec2bin(zero), equals(0));
+    });
+    test('Перевод десятичного числа '+n1024.toString()+' в двоичное ', () {
+      expect(Exercise2.DecBin.dec2bin(n1024), equals(10000000000));
+    });
+    test('Перевод десятичного числа '+(-1*n1024).toString()+' в двоичное ', () {
+      expect(()=>Exercise2.DecBin.dec2bin(-1024), throwsA(TypeMatcher<ArgumentError>()));
+    });
+    test('Перевод двоичного числа '+zero.toString()+' в десятичное ', () {
+      expect(Exercise2.DecBin.bin2dec(zero), equals(0));
+    });
+    test('Перевод двоичного числа '+b256.toString()+' в десятичное ', () {
+      expect(Exercise2.DecBin.bin2dec(b256), equals(256));
+    });
+    test('Перевод двоичного числа '+notBin.toString()+' в десятичное ', () {
+      expect(() => Exercise2.DecBin.bin2dec(notBin), throwsA(TypeMatcher<ArgumentError>()));
+    });
   });
+  //exercise3
 
-  test('String.trim() removes surrounding whitespace', () {
-    var string = '  foo ';
-    expect(string.trim(), equals('foo'));
-  });
 }
