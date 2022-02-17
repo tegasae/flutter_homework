f67f2de0d692fab63176919110cb915b7620bbbd
sae <sae@tega.ru>
2022-02-18 00:30:33 +0300

    lesson 9 continue

--- lesson9/http1/main.dart
+++ lesson9/http1/main.dart
@@ -1,12 +1,49 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 import 'package:http/retry.dart';
 
+import 'lib/hotel_json.dart';
+
+import 'dart:mirrors';
 
 main() async {
+  List<Hotel> hotelList;
   GetData getData=GetData('https://run.mocky.io/v3/ac888dc5-d193-4700-b12c-abb43e289301');
   print('hello');
+
   String str=await getData.getData();
-  //print(str);
+  print(getData.status);
+  hotelList = (json.decode(str) as List).map((i)=>Hotel.fromJson(i)).toList();
+
+  for (var i in hotelList) {
+    InstanceMirror im = reflect(i);
+    ClassMirror classMirror = im.type;
+    for (var v in classMirror.declarations.values) {
+      var name = MirrorSystem.getName(v.simpleName);
+      if (v is VariableMirror) {
+        print(name);
+        var value=im.getField(v.simpleName);
+        print(value);
+      }
+
+
+      //print(i.uuid);
+      //print(i.name);
+      //print(i.poster);
+
+    }
+    HotelDetails hotelDetails;
+    GetData getHotel=GetData('https://run.mocky.io/v3/'+i.uuid);
+    String hotelStr=await getHotel.getData();
+
+    print(getHotel.status);
+    if (getHotel.status==200) {
+      hotelDetails=HotelDetails.fromJson(json.decode(hotelStr));
+      print(hotelDetails);
+    }
+  }
+
   //Uri url = Uri.parse('http://192.168.100.1/');
   //var response = await http.get(url).timeout(Duration(seconds: 10));
   //print('Response status: ${response.statusCode}');
@@ -24,6 +61,7 @@ class GetData {
   //final String url;
   late final String json;
   late final Uri url;
+  late int status;
 
   GetData(String url) {
     this.url = Uri.parse(url);
@@ -38,7 +76,7 @@ class GetData {
       client.close();
     }
     json=response.body;
-    
+    status=response.statusCode;
     return json;
   }
 
