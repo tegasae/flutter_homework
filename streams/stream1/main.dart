48582d0d2e6a0ed6fa774922ef2e5846e3504c38
sae <sae@tega.ru>
2022-03-29 00:08:34 +0300

    stream 1

--- streams/stream1/main.dart
+++ streams/stream1/main.dart
@@ -20,32 +20,52 @@ class NumberCreator {
   Stream<int> get stream => _controller.stream;
 }
 
-void main() {
-  final myStream = countStream(10).asBroadcastStream();
+void main() async {
+  //final myStream = countStream(10).asBroadcastStream();
+  Stream<int> t=timedCounter(Duration(milliseconds: 100),10);
+  Stream<int> t1=timedCounter(Duration(milliseconds: 100),10);
+  print(await t.cast());
 
+  t1.listen((event) {event=1;});
+  await for (final i in t.skip(2).take(2).where((event) => event>2)
+                          .map((event) => (event+2))) {
+    print(i);
+  }
 
-  final subscription = myStream.listen(
-        (data) => print('Data: $data'),
-  );
+  await for (final i in t1) {
+    print(i);
+  }
+  //final subscription = myStream.listen(
+  //      (data) => print('Data: $data'),
+  //);
 
-  final subscription1 = NumberCreator().stream.listen(
-        (data) => print('Data1: $data'),
-  );
+  //final subscription1 = NumberCreator().stream.listen(
+  //      (data) => print('Data1: $data'),
+  //);
 
-  final subscription2 = myStream.listen(
-        (data) {
-      print('Data2: $data');
-    },
-    onError: (err) {
-      print('Error!');
-    },
-    cancelOnError: false,
-    onDone: () {
-      print('Done!');
-    },
-  );
-  print('hello');
-  print(subscription);
+  //final subscription2 = myStream.listen(
+  //      (data) {
+  //    print('Data2: $data');
+  //  },
+  //  onError: (err) {
+  //    print('Error!');
+  //  },
+  //  cancelOnError: false,
+  //  onDone: () {
+  //    print('Done!');
+  //  },
+  //);
+  //print('hello');
+  //print(subscription);
 
+}
+
+Stream<int> timedCounter(Duration interval, [int? maxCount]) async* {
+  int i = 0;
+  while (true) {
+    await Future.delayed(interval);
+    yield i++;
+    if (i == maxCount) break;
+  }
 }
 
